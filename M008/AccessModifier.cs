eb3d362a80f74866fc0de791a48f776d75ce5a98
ppedvLukasK <lukask@ppedv.de>
2023-05-10 16:53:10 +0200

    M008

---
+++ M008/AccessModifier.cs
@@ -0,0 +1,17 @@
+﻿namespace M008;
+
+class AccessModifier //Klassen und Member ohne Modifier sind Internal
+{
+	public string Name { get; set; } //Kann überall gesehen werden (auch außerhalb vom Projekt)
+
+	private int Alter { get; set; } //Kann nur in dieser Klasse gesehen werden
+
+	internal string Wohnort { get; set; } //Kann überall gesehen werden (nur im Projekt)
+
+
+	protected string Lieblingsfarbe { get; set; } //Nur in dieser Klasse und in Unterklasse (auch außerhalb vom Projekt)
+
+	protected internal string Lieblingsnahrung { get; set; } //Kann überall im Projekt gesehen werden (internal) und in Unterklassen auch außerhalb vom Projekt
+
+	protected private DateTime Geburtsdatum { get; set; } //Kann nur in dieser Klasse und in Unterklassen nur im Projekt gesehen werden
+}
---
+++ M008/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace M008;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Mensch m = new Mensch("Max", 12);
+        Console.WriteLine(m.Name); //Name wird weitergegeben
+        m.WasBinIch(); //WasBinIch wird weitergegeben
+
+		Kind k = new Kind("Max", 12);
+        Console.WriteLine(k.Name); //Name wird weitergegeben
+		Console.WriteLine(k.Alter); //Alter wird weitergegeben
+
+		Console.WriteLine(m.ToString()); //M008.Mensch
+    }
+}
+
+public class Lebewesen //Basisklasse
+{
+	public string Name { get; set; }
+
+    public Lebewesen(string name)
+    {
+		Name = name;
+    }
+
+	/// <summary>
+	/// virtual: Methode kann in Unterklassen überschrieben werden (normalerweise können Methoden nicht überschrieben werden)
+	/// </summary>
+    public virtual void WasBinIch()
+	{
+        Console.WriteLine("Ich bin ein Lebewesen");
+    }
+
+	public override string ToString()
+	{
+		return $"Ich bin ein {base.ToString()} und mein Name ist {Name}";
+	}
+}
+
+public class Mensch : Lebewesen
+{
+	public int Alter { get; set; }
+
+	public Mensch(string name, int alter) : base(name) //Konstruktor von oben muss mit base(...) verkettet werden
+	{ //hier bei Parametern einfach alter hinzufügen
+		Alter = alter;
+	}
+
+	/// <summary>
+	/// Spezifische Implementation in der Mensch Klasse für WasBinIch()
+	/// </summary>
+	public override void WasBinIch()
+	{
+        Console.WriteLine("Ich bin ein Mensch");
+		base.WasBinIch(); //base: selbiges wie this, nur im Vererbungskontext (ruft die Methode in der Oberklasse auf)
+    }
+
+	public override sealed string ToString()
+	{
+		return base.ToString() + $" und bin {Alter} Jahre alt";
+	}
+}
+
+public sealed class Kind : Mensch //sealed: Kann nicht weitervererben
+{
+	public Kind(string name, int alter) : base(name, alter)
+	{
+
+	}
+
+	//public override string ToString()
+	//{
+	//	return base.ToString();
+	//}
+}
